4244d6c9619d3d1fb455df6dd204ca4010af2826
[Jeremy Grunert] <[jgrnrt@gmail.com]>
2021-10-18 22:16:35 +0200

    Version 1.3

--- src/de/medieninformatik/softwaretechnik/Main.java
+++ src/de/medieninformatik/softwaretechnik/Main.java
@@ -3,7 +3,7 @@ package de.medieninformatik.softwaretechnik;
 /**
  * @author Karsten Kurella m29278
  * @author Jeremy Grunert m29265
- * date: 2021-11-10
+ * date: 2021-18-10
  * Softwaretechnik - Aufgabe 1
  * https://github.com/jgrnrt/Metrik
 */
